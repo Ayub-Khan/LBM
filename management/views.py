753804558c94eb111b53ef32993d7f5c23063f27
quratulain <quratulain.stylesage@gmail.com>
2019-06-14 01:31:30 +0500

    LBM-4: authentication added.

--- management/views.py
+++ management/views.py
@@ -2,5 +2,9 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 # Create your views here.
+from django.template import loader
+
+
 def index(request):
-    return HttpResponse('Hello World')
+    template = loader.get_template('home.html')
+    return HttpResponse(template.render({}, request))
