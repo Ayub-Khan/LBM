ade70c27a672af269d2cdbf9124e3d5abf469319
quratulain <quratulain.stylesage@gmail.com>
2019-06-16 16:55:42 +0500

    Login, Signup and Password Reset added.

--- LBM/urls.py
+++ LBM/urls.py
@@ -19,7 +19,7 @@ from django.views.generic import TemplateView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('accounts/', include('django.contrib.auth.urls')),
-    path(r'', include('management.urls')),
-    # path('', TemplateView.as_view(template_name='home.html'), name='home'),    # new
+    path('management/', include('management.urls')),
+    path('management/', include('django.contrib.auth.urls')),
+    path('', TemplateView.as_view(template_name='home.html'), name='home'),
 ]
--- management/urls.py
+++ management/urls.py
@@ -1,8 +1,11 @@
+from django.urls import path
 from django.conf.urls import url
 from django.conf.urls.static import static
 
 from management import views
 
 urlpatterns = [
-    url(r'^$', views.index, name='index'),
+    # url(r'^$', views.index, name='index'),
+    # url(r'signup/', views.SignUP, name='signup'),
+    path('signup/', views.SignUp.as_view(), name='signup')
 ]
