8b0993c3b78314dc1a67517dcc6e87cf3e41ac17
quratulain <quratulain.stylesage@gmail.com>
2019-08-04 20:51:12 +0500

    Theme added.

--- management/urls.py
+++ management/urls.py
@@ -1,6 +1,10 @@
 from django.urls import path
 from django.views.generic import TemplateView
+from django.conf.urls.static import static
 
+from LBM import settings
+
+app_name = 'management'
 urlpatterns = [
     path('', TemplateView.as_view(template_name='dashboard.html'), name='dashboard'),
-]
+] + static(settings.STATIC_URL)
