1ea333f4c7b2ff9d3ab13dd4cbc6f663c56b4d7b
quratulain <quratulain.stylesage@gmail.com>
2019-08-26 00:13:54 +0500

    Dashboard custimzed and login check added.
    base_html improved.
    
    identation improved.

--- management/urls.py
+++ management/urls.py
@@ -1,10 +1,17 @@
 from django.urls import path
 from django.views.generic import TemplateView
 from django.conf.urls.static import static
+from django.contrib.auth.decorators import login_required
 
 from LBM import settings
 
 app_name = 'management'
 urlpatterns = [
-    path('', TemplateView.as_view(template_name='dashboard.html'), name='dashboard'),
+
+    path(
+        '',
+        login_required(TemplateView.as_view(template_name='home_page.html')),
+        name='dashboard'
+    ),
+
 ] + static(settings.STATIC_URL)
