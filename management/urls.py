bd5f390cc08137058e0c50d0d78b8fea5b512c7f
quratulain25 <quratulain@arbisoft.com>
2019-09-26 22:05:23 +0500

    Dashboard custimzed and login check added.

--- management/urls.py
+++ management/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path
-from django.views.generic import TemplateView
 from django.conf.urls.static import static
 
 from LBM import settings
+from management import views
 
 app_name = 'management'
 urlpatterns = [
-    path('', TemplateView.as_view(template_name='dashboard.html'), name='dashboard'),
-] + static(settings.STATIC_URL)
+                  path('', views.dashboard_view, name='dashboard'),
+                  path('view_client', views.client_profile_view, name='view_client'),
+                  path('create_client', views.client_registration_view, name='create_client')
+              ] + static(settings.STATIC_URL)
--- management/views.py
+++ management/views.py
@@ -1,8 +1,37 @@
-from django.http import HttpResponse
-from django.template import loader
+from django.contrib.auth.decorators import login_required
+from django.shortcuts import render
+from django.views.generic import TemplateView, View
 
 
-# Not required yet, as using generic view
-# def home(request):
-#     template = loader.get_template('home.html')
-#     return HttpResponse(template.render({}, request))
+class DashboardView(TemplateView):
+    template_name = 'home_page.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['title'] = 'LBM'
+        return context
+
+
+dashboard_view = login_required(DashboardView.as_view())
+
+
+class ClientProfileView(TemplateView):
+    template_name = 'client_management/view_client_profile.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['title'] = 'LBM'
+        return context
+
+
+client_profile_view = login_required(ClientProfileView.as_view())
+
+
+class ClientRegistrationView(View):
+    template_name = 'client_management/create_client_profile.html'
+
+    def get(self, request):
+        return render(request, self.template_name, {'title': 'Client Management'})
+
+
+client_registration_view = login_required(ClientRegistrationView.as_view())
