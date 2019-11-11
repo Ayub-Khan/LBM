535316b40581d8b21e0b3258f855d24f34a83aa4
Ayub khan <muhammadayubkhan6@gmail.com>
2019-11-11 20:20:12 +0500

    -heroku-deployment -readme

--- LBM/settings.py
+++ LBM/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 
 import os
 
+import django_heroku
+
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -118,7 +120,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
+PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(PROJECT_ROOT, 'static')
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static')
 ]
@@ -128,3 +132,5 @@ LOGOUT_REDIRECT_URL = '/'
 
 EMAIL_BACKEND = "django.core.mail.backends.filebased.EmailBackend"
 EMAIL_FILE_PATH = os.path.join(BASE_DIR, "sent_emails")
+
+django_heroku.settings(locals())
---
+++ README.rst
@@ -0,0 +1,13 @@
+***************
+LBM
+***************
+LBM is a Local Bussiness Management System.
+This is a Django Training Project.
+
+Live 
+------------------
+This project is live on Heroku. You can use the following credentials to access it.
+
+- `LBM <https://boiling-lake-58542.herokuapp.com/>`_
+- Username : 'test'
+- Password : 'testtest'
