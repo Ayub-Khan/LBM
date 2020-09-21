60ddd70c75389bc745a6e64eb57609ab6970c347
quratulain25 <quratulain@arbisoft.com>
2020-09-21 19:05:36 +0500

    Company/CRUD added.

--- management/admin.py
+++ management/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
+from management.models import Company, Product, ImportExportEvent
+
 # Register your models here.
+admin.site.register(Company)
+admin.site.register(Product)
+admin.site.register(ImportExportEvent)
--- management/models.py
+++ management/models.py
@@ -1,21 +1,36 @@
+from django.contrib.auth.models import User
 from django.db import models
+from django.urls import reverse_lazy
 
 
-# Create your models here.
 class Product(models.Model):
+    def __str__(self):
+        return self.title
+
     title = models.CharField(max_length=30)
-    date_created = models.DateField()
+    date_created = models.DateField(auto_now_add=True, editable=True)
 
 
 class Company(models.Model):
+    def __str__(self):
+        return self.name
+
+    def get_absolute_url(self):
+        return reverse_lazy('management:list_companies')
+
+    COMPANY_TYPES = (
+        ('v', 'Vendor'),
+        ('c', 'Client'),
+    )
+    company_type = models.CharField(max_length=1, choices=COMPANY_TYPES)
+
     name = models.CharField(max_length=30)
     address = models.CharField(max_length=60)
-    contact = models.CharField(max_length=20)
-    date_created = models.DateField()
-    client_type = models.CharField(max_length=20)
+    contact = models.CharField(max_length=30)
+    date_created = models.DateField(auto_now_add=True, editable=True)
 
 
-class ImportExportEvents(models.Model):
+class ImportExportEvent(models.Model):
     product_id = models.ForeignKey(Product, on_delete=models.CASCADE)
     company_id = models.ForeignKey(Company, on_delete=models.CASCADE)
     expense_description = models.CharField(max_length=200)
@@ -27,4 +42,4 @@ class ImportExportEvents(models.Model):
     total_imported = models.IntegerField()
     cost = models.IntegerField()
     remaining_products = models.IntegerField()
-    date_created = models.DateField()
+    date_created = models.DateField(auto_now_add=True, editable=True)
