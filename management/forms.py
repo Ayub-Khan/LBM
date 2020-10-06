22a42b5610bc94bb9eab73d191ebfa9b379e17f8
quratulain25 <quratulain@arbisoft.com>
2020-10-06 10:45:08 +0500

    rebased from master.

--- management/forms.py
+++ management/forms.py
@@ -1,14 +1,8 @@
 from django import forms
-from management.models import Company, Product
+from management.models import Company
 
 
 class CompanyForm(forms.ModelForm):
     class Meta:
         model = Company
-        fields = '__all__'
-
-
-class ProductForm(forms.ModelForm):
-    class Meta:
-        model = Product
         fields = '__all__'
